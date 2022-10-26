2627267a5d39ec1771aaaa13b05c4802714641d9
Huynh Duc Dung <dunghd.it@gmail.com>
2022-10-27 03:54:07 +0800

    :sparkles: add nextjs 13 with turbo demo

--- apps/vite-counter/src/machines/counterMachine.ts
+++ apps/vite-counter/src/machines/counterMachine.ts
@@ -18,6 +18,7 @@ export const counterMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QGMD2BXAdgFzAJwAIBbAQ2QAsBLTMAOjO0oDcwBiASQDkBhRUAB1SxKjVJj4gAHogC0ARgBMABloBWAJyqFcpXIDsADgBsAFgWqANCACesueoO0DiuWYDMJ-RtUBfH1bQsXEJSCmo6BmY2ABEAUV4kEEFhUXFE6QR5ZTVNbV1DU3MrW0yFEyMnF3dPPW8-fxBMVAg4CUCcfGIyKhp6ZEYWCWSRSjEJDJk3HVo5VTl5gzdnPSNa4rsHSu1qr00-AIwOkO7woaERsfTZNw8ZuYWl-VXLG2v7Ldc9N00jc3U9eo+IA */
   createMachine(
     {
+      predictableActionArguments: true,
       context: { count: 0 },
       tsTypes: {} as import("./counterMachine.typegen").Typegen0,
       schema: {
