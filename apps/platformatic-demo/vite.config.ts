56c2c681e3042c93929b684fa61cdb4308af360c
Huynh Duc Dung <dunghd.it@gmail.com>
2022-10-30 22:51:34 +0800

    :white_check_mark: ignore ts error on vitest for platformatic

--- apps/platformatic-demo/vite.config.ts
+++ apps/platformatic-demo/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-expect-error Object literal may only specify known properties, and 'test' does not exist in type 'UserConfigExport'.
   test: {
     globals: true,
   },
